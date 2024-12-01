7d89f68ead46aa05bdf96e56d2b40f4f5f27baa7
Eneye-cyber <alaoeneye@gmail.com>
2024-12-01 12:22:56 +0100

    login state verification

--- oua-atl-web/app/(public)/layout.tsx
+++ oua-atl-web/app/(public)/layout.tsx
@@ -1,15 +1,19 @@
 import TopBar from '@/app/ui/shared/TopBar'
 import NavBar from '@/app/ui/shared/NavBar'
 import Footer from '@/app/ui/shared/Footer'
+import { headers } from 'next/headers';
+
 
 export default function PublicLayout({ children }: {
   children: React.ReactNode
 }) {
+  const headersList = headers();
+  const role = headersList.get('x-custom-role') ?? null;
   return (
     <>
     <div className='flex-column h-full min-h-screen'>
       <header aria-label="page-header" className='mb-uto'>
-        <TopBar />
+        <TopBar role={role} />
         <NavBar />
       </header>
 
